6fe99fe92b775586a1cdee6564d6683b92ffe1b1
Avzel <65988622+Avzel@users.noreply.github.com>
2022-11-03 21:15:04 +0800

    Implement game loop

---
+++ RiichiNET.Core/Riichi.cs
@@ -0,0 +1,12 @@
+namespace RiichiNET.Core;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using RiichiNET.Core.Components;
+
+public sealed class Riichi
+{
+    // TODO:
+}
