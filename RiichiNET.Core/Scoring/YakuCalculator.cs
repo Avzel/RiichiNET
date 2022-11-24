cd0ebef08599f7df9fe83259dd96447d0bcdd7a6
Avzel <65988622+Avzel@users.noreply.github.com>
2022-11-24 16:21:49 +0800

    Define Yaku

--- RiichiNET.Core/Scoring/YakuCalculator.cs
+++ RiichiNET.Core/Scoring/YakuCalculator.cs
@@ -275,7 +275,11 @@ internal sealed class YakuCalculator
 			) count++;
         }
 		if (count == 3) _current.Add(Yaku.SanAnkou);
-		else if (count == 4) _yakuman.Add(Yaku.SuuAnkou);
+		else if (count == 4)
+        {
+			if (_player.WinningTiles.Count() == 1) _yakuman.Add(Yaku.SuuAnkouSingleWait);
+            else _yakuman.Add(Yaku.SuuAnkou);
+        }
     }
 
 	private void SanshokuDoukou(WinningHand wh)
@@ -343,39 +347,75 @@ internal sealed class YakuCalculator
         }
     }
 
-	private void HonIisou(WinningHand wh)
+	private void Iisou(WinningHand wh)
 	{
-		// TODO:
-	}
-
-	private void ChinIisou(WinningHand wh)
-	{
-		// TODO:
-	}
+        bool honor = false;
+        HashSet<Suit> suits = new HashSet<Suit>();
+		foreach (Meld meld in wh.GetAllMelds())
+		{
+			if (meld.OnlyHonors()) honor = true;
+			else suits.Add(meld[0].GetSuit());
+        }
+		if (honor && suits.Count() == 1)
+		{
+			if (_player.IsOpen()) _current.Add(Yaku.HonIisouOpen);
+			else _current.Add(Yaku.HonIisouClosed);
+        }
+		else if (!honor && suits.Count() == 1)
+		{
+			if (_player.IsOpen()) _current.Add(Yaku.ChinIisouOpen);
+			else _current.Add(Yaku.ChinIisouClosed);
+		}
+    }
 
 	private void KokushiMusou(WinningHand wh)
 	{
-		// TODO:
-	}
+		if (wh.GetAllMelds().Count() == 1) _yakuman.Add(Yaku.KokushiMusou);
+    }
 
-	private void ShouSuushii(WinningHand wh)
+	private void Suushii(WinningHand wh)
 	{
-		// TODO:
+		if (wh.Count(Mentsu.Koutsu, Mentsu.Kantsu) > 1)
+        {
+            HashSet<Value> values = new HashSet<Value>();
+            foreach (Meld meld in wh.GetMelds(Mentsu.Jantou, Mentsu.Koutsu, Mentsu.Kantsu))
+            {
+                if (meld.OnlyWinds()) values.Add(meld[0]);
+            }
+            if (values.Count() == 4)
+			{
+				if (wh.GetMelds(Mentsu.Jantou)[0].OnlyWinds())
+                    _yakuman.Add(Yaku.ShouSuushii);
+				else _yakuman.Add(Yaku.DaiSuuShii);
+            }
+        }
 	}
 
 	private void TsuuIisou(WinningHand wh)
 	{
-		// TODO:
-	}
+		foreach (Meld meld in wh.GetAllMelds())
+		{
+			if (!meld.OnlyHonors()) return;
+        }
+        _yakuman.Add(Yaku.TsuuIisou);
+    }
 
 	private void ChinRoutou(WinningHand wh)
 	{
-		// TODO:
+		foreach (Meld meld in wh.GetAllMelds())
+		{
+			if (!meld.OnlyTerminals()) return;
+        }
+        _yakuman.Add(Yaku.ChinRoutou);
 	}
 
 	private void RyuuIisou(WinningHand wh)
 	{
-		// TODO:
+		foreach (Meld meld in wh.GetAllMelds())
+		{
+			if (!meld.OnlyGreens()) return;
+        }
+        _yakuman.Add(Yaku.RyuuIisou);
 	}
 
 	private void TenHou()
@@ -410,11 +450,30 @@ internal sealed class YakuCalculator
 
 	private void ChuurenPoutou(WinningHand wh)
 	{
-		// TODO:
-	}
+        if (!_player.IsOpen())
+        {
+            HashSet<Suit> suits = new HashSet<Suit>();
+            foreach (Meld meld in wh.GetAllMelds())
+            {
+                if (meld.OnlyHonors()) return;
+                else suits.Add(meld[0].GetSuit());
+            }
 
-	private void DaiSuuShii(WinningHand wh)
-	{
-		// TODO:
-	}
+            if (
+                suits.Count() == 1 &&
+                _player.Hand.ValueCount((int)suits.First() + 1) is 3 or 4 &&
+                _player.Hand.ValueCount((int)suits.First() + 9) is 3 or 4 &&
+                _player.Hand.ValueCount((int)suits.First() + 2) is 1 or 2 &&
+                _player.Hand.ValueCount((int)suits.First() + 3) is 1 or 2 &&
+                _player.Hand.ValueCount((int)suits.First() + 4) is 1 or 2 &&
+                _player.Hand.ValueCount((int)suits.First() + 5) is 1 or 2 &&
+                _player.Hand.ValueCount((int)suits.First() + 6) is 1 or 2 &&
+                _player.Hand.ValueCount((int)suits.First() + 7) is 1 or 2 &&
+                _player.Hand.ValueCount((int)suits.First() + 8) is 1 or 2
+            ){
+				if (_player.WinningTiles.Count() == 9) _yakuman.Add(Yaku.JunseiChuurenPoutou);
+				else _yakuman.Add(Yaku.ChuurenPoutou);
+            }
+        }
+    }
 }
